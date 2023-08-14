2f59ca90d47650396816b86da546c083e5c43d6e
Bartkooo <bartosz.plochocki@gmail.com>
2023-08-14 16:45:24 +0200

    post user association

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  root 'home#index'
+  root 'posts#index'
 
   devise_for :users, :controllers => { :registrations => "registrations" }
   resources :users, only: [:show]
