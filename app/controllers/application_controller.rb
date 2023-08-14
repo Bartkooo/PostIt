4eae7bc154f98e4913f16546150ee7b299994969
Bartkooo <bartosz.plochocki@gmail.com>
2023-08-14 11:26:02 +0200

    add last and first name to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :remove_authentication_flash_message_if_root_url_requested
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,5 +9,13 @@ class ApplicationController < ActionController::Base
     if session[:user_return_to] == root_path and flash[:alert] == I18n.t('devise.failure.unauthenticated')
       flash[:alert] = nil
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
   end
 end
