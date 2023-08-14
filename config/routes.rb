d5a3b9a193c7679b531b25abf3f4c77ba0f076c0
Bartkooo <bartosz.plochocki@gmail.com>
2023-08-14 12:05:19 +0200

    user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users, :controllers => { :registrations => "registrations" }
+  match 'users/:id' => 'users#show', via: :get
 end
