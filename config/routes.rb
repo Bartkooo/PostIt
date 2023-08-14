5a684327dcf63b3b08e3f424e3362157f921b73c
Bartkooo <bartosz.plochocki@gmail.com>
2023-08-14 12:43:00 +0200

    link to profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users, :controllers => { :registrations => "registrations" }
-  match 'users/:id' => 'users#show', via: :get
+  resources :users, only: [:show]
 end
