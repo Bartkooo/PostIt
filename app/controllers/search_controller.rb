27d9bdf37a7d5746bf6a4b5c37cb4dec7d950bfb
Bartkooo <bartosz.plochocki@gmail.com>
2023-11-15 18:32:45 +0100

    display only if param

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
   def index
     key = "%#{params[:search]}%"
-    @users = User.where("first_name LIKE ? OR last_name LIKE ?", key, key)
+    @users = ''
+
+    if params[:search] != ''
+      @users = User.where("first_name LIKE ? OR last_name LIKE ?", key, key)
+    end
   end
 end
