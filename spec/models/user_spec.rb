4c3c98d9a20608fd3192197329659477c0ace1a0
Bartkooo <bartosz.plochocki@gmail.com>
2023-11-27 19:42:53 +0100

    simple rspecs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "can run model tests" do
-    expect(false).to be(false)
+  let(:user1) {
+    User.new(first_name: "test", last_name:"testing", email:'test@g.com', password:'test123')
+  }
+
+  it "does not create user without valid values" do
+    expect(User.new).to_not be_valid
+  end
+
+  it "does create valid user" do
+    expect(user1).to be_valid
   end
 end
