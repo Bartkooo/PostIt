a03c8fe4a0d08ed0caa190835bf86e72d7460212
Bartkooo <bartosz.plochocki@gmail.com>
2023-12-27 20:35:08 +0100

    input animation with stimulus

---
+++ app/javascript/controllers/empty_input_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="empty-input"
+export default class extends Controller {
+  static targets = [ 'input', 'label' ]
+
+  update() {
+    if (this.inputTarget.value != '') {
+      this.labelTarget.classList.add('form__label-notempty')
+    } else if (this.inputTarget.value == '') {
+      this.labelTarget.classList.remove('form__label-notempty')
+    }
+  }
+}
